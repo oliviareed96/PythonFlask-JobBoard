d9cf8c4b40816e0ea3c2c2fae2c0844bd11bad74
Olivia Reed <olivia.reed@kpmg.co.uk>
2020-02-25 16:46:51 +0000

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, "_connection", None)
     if connection == None:
-        connection, g._connection = sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -19,13 +19,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-        cursor.close()
+    
+    cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
-    if connection != None:
+    if connection is not None:
         connection.close()
 
 @app.route('/')
