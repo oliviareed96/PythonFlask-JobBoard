6b257ab2d828ab852f003a1748427c182e5c5d14
Olivia Reed <olivia.reed@kpmg.co.uk>
2020-02-25 14:55:26 +0000

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/jobs')
 def jobs():
-    render_template('index.html')
+    return render_template('index.html')
